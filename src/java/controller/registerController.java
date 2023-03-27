132f1f3ddae6d9b6f612e3a74e2b63982a37bf58
Sandaruwan <dsrgotest@gmail.com>
2023-03-28 01:44:00 +0530

    do some register form input data validations

--- src/java/controller/registerController.java
+++ src/java/controller/registerController.java
@@ -7,6 +7,8 @@ package controller;
 import beans.DatabaseConnection;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;  
+import java.security.MessageDigest;  
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -20,7 +22,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name = "registercontroller", urlPatterns = {"/registerController"})
+
+@WebServlet(name = "registerController", urlPatterns = {"/registerController"})
 public class registerController extends HttpServlet {
  
     Connection con;
@@ -38,37 +41,83 @@ public class registerController extends HttpServlet {
         String userNic = request.getParameter("userNic");
         String userPwd = request.getParameter("userPwd");
         String userRole = "user";
+        String errorMsg = null;
 
-        PrintWriter out = response.getWriter();   
-//        out.println(userName); check data catch
-//        out.println(userEmail);
-//        out.println(userPhone);
-//        out.println(userNic);
-//        out.println(userPwd);
-
-        Connection con;
-        try {
-            con = DatabaseConnection.connectToDatabase ("jdbc:mysql://localhost/abc_university_p","root","");
-        
-            pst = con.prepareStatement("INSERT INTO users(userName,userEmail,userPhone,userNic,userPwd,userRole) VALUES (?,?,?,?,?,?)");
-
-            pst.setString(1,userName);
-            pst.setString(2,userEmail);
-            pst.setInt(3,Integer.parseInt(userPhone));
-            pst.setString(4,userNic);
-            pst.setString(5,userPwd);
-            pst.setString(6,userRole);
-            pst.executeUpdate();
-
-        } catch (SQLException ex) {
-            Logger.getLogger(registerController.class.getName()).log(Level.SEVERE, null, ex);
-            out.println("<h1> Somthing Went Wrong !!! </h1>");
+         // Server-side form validation
+        if (userName == null || userName.trim() .isEmpty() ) {
+            errorMsg = "Please enter a valid user name.";
+        } else if (userEmail == null || !userEmail.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")) {
+            errorMsg = "Please enter a valid email address.";
+        } else if (userPhone == null || !userPhone.matches("^\\d{10}$")) {
+            errorMsg = "Please enter a valid phone number.";
+        } else if (userNic == null || !userNic.matches("^\\d{9}[Vv]$")) {
+            errorMsg = "Please enter a valid NIC number.";
+        } else if (userPwd == null || userPwd.length() < 8 ||  userPwd.trim().isEmpty() ) {
+            errorMsg = "Please enter a valid password.";
         }
-        //response.sendRedirect("login.jsp");
-       // out.println("<h1> DONE !!! </h1>");
-       request.setAttribute("Message", "Hello " + userName + " your registration is successful. Now"
-                + " you can login to the system");
-        request.getRequestDispatcher("login.jsp").forward(request, response);
 
+        if (errorMsg == null) {
+        
+        
+            
+        
+                PrintWriter out = response.getWriter();   
+                Connection con;
+                try {
+                    con = DatabaseConnection.connectToDatabase ("jdbc:mysql://localhost/abc_university_p","root","");
+                    
+                    // Password encryption using MD5
+                     try {  
+                            MessageDigest md = MessageDigest.getInstance("MD5");
+                            md.update(userPwd.getBytes());
+                            byte[] digest = md.digest();
+                            StringBuilder sb = new StringBuilder();
+                            for (byte b : digest) {
+                                sb.append(String.format("%02x", b & 0xff));
+                            }
+                            String encryptedPwd = sb.toString();
+
+                    }   
+                    catch (NoSuchAlgorithmException e)   
+                    {  
+                        e.printStackTrace();  
+                    }  
+                    
+                    
+                    
+                    
+                    pst = con.prepareStatement("INSERT INTO users(userName,userEmail,userPhone,userNic,userPwd,userRole) VALUES (?,?,?,?,?,?)");
+                    pst.setString(1,userName);
+                    pst.setString(2,userEmail);
+                    pst.setInt(3,Integer.parseInt(userPhone));
+                    pst.setString(4,userNic);
+                    pst.setString(5,userPwd);
+//                    pst.setString(5,encryptedPwd);
+                    pst.setString(6,userRole);
+                    pst.executeUpdate();
+
+                } catch (SQLException ex) {
+                    Logger.getLogger(registerController.class.getName()).log(Level.SEVERE, null, ex);
+                    out.println("<h1> Somthing Went Wrong !!! </h1>");
+                    return;
+                }
+                //response.sendRedirect("login.jsp");
+               // out.println("<h1> DONE !!! </h1>");
+               request.setAttribute("Message", "Hello " + userName + " your registration is successful. Now"
+                        + " you can login to the system");
+                request.getRequestDispatcher("login.jsp").forward(request, response);
+
+        
+        
+        } else {
+            request.setAttribute("errorMsg", errorMsg);
+            
+            
+            response.setContentType("text/html; charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<script type='text/javascript'> alert ('check your Search Condition'); </script>");
+            request.getRequestDispatcher("register.jsp").forward(request, response);
+                     
+        }    
     }
 }
