b695ad84eb3e032a65f3779f824bf51d0df17557
Sandaruwan <dsrgotest@gmail.com>
2023-03-28 01:43:24 +0530

    do some login form  input validations

--- src/java/controller/loginController.java
+++ src/java/controller/loginController.java
@@ -37,46 +37,68 @@ public class loginController extends HttpServlet {
             throws ServletException, IOException {
         String userEmail = request.getParameter("userEmail");
         String userPwd = request.getParameter("userPwd");
-       
-        PrintWriter out = response.getWriter();
-        Connection con;
-        try {
-            con = DatabaseConnection.connectToDatabase("jdbc:mysql://localhost/abc_university_p","root","");
-            pst = con.prepareStatement("SELECT * FROM users WHERE userEmail=? AND userPwd=?");
+        String errorMsg = null;
         
-            pst.setString(1, userEmail);
-            pst.setString(2, userPwd); 
-            ResultSet rs = pst.executeQuery();
-            if(rs.next()){
-                String userRole = rs.getString("userRole");
-                HttpSession session = request.getSession();
-//                session.setAttribute("UE", userEmail);
-                
-                 if(userRole.equals("admin")) {
-                    session.setAttribute("admin", userEmail);
-                    request.setAttribute("message","Hello admin " + userEmail);
-                    response.sendRedirect("admin.jsp");
-                } else {
-                    session.setAttribute("user", userEmail);
-                    request.setAttribute("message","Hello " + userEmail);
-                    response.sendRedirect("home.jsp");
-                } 
-
-  
-                
-            }else{
-                 request.setAttribute("Message", "Login Failed please check your User Email and Password");
-                 request.getRequestDispatcher("login.jsp").forward(request, response);
-            }
-     
-            
-         } catch (SQLException ex) {
-            Logger.getLogger(registerController.class.getName()).log(Level.SEVERE, null, ex);
-            out.println("<h1>Something Went Wrong!!!</h1>");
+        if (userEmail == null || !userEmail.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")) {
+            errorMsg = "Please enter a valid email address.";
+        } else if (userPwd == null || userPwd.length() < 8 ||  userPwd.trim().isEmpty() ) {
+            errorMsg = "Please enter a valid password.";
         }
+
+        if (errorMsg == null) {
+        
+        
+        
+        
+            PrintWriter out = response.getWriter();
+            Connection con;
+            try {
+                con = DatabaseConnection.connectToDatabase("jdbc:mysql://localhost/abc_university_p","root","");
+                pst = con.prepareStatement("SELECT * FROM users WHERE userEmail=? AND userPwd=?");
+
+                pst.setString(1, userEmail);
+                pst.setString(2, userPwd); 
+                ResultSet rs = pst.executeQuery();
+                if(rs.next()){
+                    String userRole = rs.getString("userRole");
+                    HttpSession session = request.getSession();
+    //                session.setAttribute("UE", userEmail);
+
+                     if(userRole.equals("admin")) {
+                        session.setAttribute("admin", userEmail);
+                        request.setAttribute("message","Hello admin " + userEmail);
+                        response.sendRedirect("admin.jsp");
+                    } else {
+                        session.setAttribute("user", userEmail);
+                        request.setAttribute("message","Hello " + userEmail);
+                        response.sendRedirect("home.jsp");
+                    } 
+
+
+
+                }else{
+                     request.setAttribute("Message", "Login Failed please check your User Email and Password");
+                     request.getRequestDispatcher("login.jsp").forward(request, response);
+                }
+
+
+             } catch (SQLException ex) {
+                Logger.getLogger(registerController.class.getName()).log(Level.SEVERE, null, ex);
+                out.println("<h1>Something Went Wrong!!!</h1>");
+            }
        
-    }
+        } else {
+            request.setAttribute("errorMsg", errorMsg);
+            
+            
+            response.setContentType("text/html; charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<script type='text/javascript'> alert ('check your Search Condition'); </script>");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+                     
+        }    
 
     
-
-    }
+    
+}
+}
