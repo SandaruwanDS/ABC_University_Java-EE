1dc10e5202f69649ba7e237dc79dfd3bf639d30b
Sandaruwan <dsrgotest@gmail.com>
2023-05-21 14:41:00 +0530

    update login controller

--- src/java/controller/loginController.java
+++ src/java/controller/loginController.java
@@ -4,8 +4,8 @@
  * and open the template in the editor.
  */
 package controller;
-
-import beans.DatabaseConnection;
+import util.PasswordEncryptor;
+import bean.DatabaseConnection;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.MessageDigest;
@@ -41,41 +41,15 @@ public class loginController extends HttpServlet {
         String userPwd = request.getParameter("userPwd");
         String errorMsg = null;
         String encryptedPwd = null;
-        
-        if (userEmail == null || !userEmail.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")) {
-            errorMsg = "Please enter a valid email address.";
-        } else if (userPwd == null || userPwd.length() < 8 ||  userPwd.trim().isEmpty() ) {
-            errorMsg = "Please enter a valid password.";
-        }
 
-        if (errorMsg == null) {
-        
-        
-        
-        
             PrintWriter out = response.getWriter();
             Connection con;
             try {
-                con = DatabaseConnection.connectToDatabase("jdbc:mysql://localhost/abc_university_p","root","");
-                
                 // Password encryption using MD5
-                     try {  
-                            MessageDigest md = MessageDigest.getInstance("MD5");
-                            md.update(userPwd.getBytes());
-                            byte[] digest = md.digest();
-                            StringBuilder sb = new StringBuilder();
-                            for (byte b : digest) {
-                                sb.append(String.format("%02x", b & 0xff));
-                            }
-                            encryptedPwd = sb.toString();
-                    }   
-                    catch (NoSuchAlgorithmException e)   
-                    {  
-                        e.printStackTrace();  
-                    }  
-                
-                
-                
+                encryptedPwd = PasswordEncryptor.encryptPassword(userPwd);
+
+                con = DatabaseConnection.connectToDatabase("jdbc:mysql://localhost/abc_university_p","root","");
+
                 pst = con.prepareStatement("SELECT * FROM users WHERE userEmail=? AND userPwd=?");
 
                 pst.setString(1, userEmail);
@@ -84,17 +58,24 @@ public class loginController extends HttpServlet {
                 ResultSet rs = pst.executeQuery();
                 if(rs.next()){
                     String userRole = rs.getString("userRole");
+                    String userName = rs.getString("userName");
                     HttpSession session = request.getSession();
-    //                session.setAttribute("UE", userEmail);
+                    session.setAttribute("UN", userName);
 
                      if(userRole.equals("admin")) {
-                        session.setAttribute("admin", userEmail);
-                        request.setAttribute("message","Hello admin " + userEmail);
-                        response.sendRedirect("admin.jsp");
+                         session.setAttribute("admin", userName);
+                         request.setAttribute("message", "Hello admin " + userName);
+
+                         out.println("<script type='text/javascript'>");
+                         out.println("alert('Your login Success');");
+                         out.println("location='login.jsp'");
+                         out.println("</script>");
+
+                         response.sendRedirect("admin/admin.jsp");
                     } else {
-                        session.setAttribute("user", userEmail);
-                        request.setAttribute("message","Hello " + userEmail);
-                        response.sendRedirect("home.jsp");
+                         session.setAttribute("user", userName);
+                         request.setAttribute("message", "Hello " + userName);
+                         response.sendRedirect("user/home.jsp");
                     } 
 
 
@@ -110,16 +91,16 @@ public class loginController extends HttpServlet {
                 out.println("<h1>Something Went Wrong!!!</h1>");
             }
        
-        } else {
-            request.setAttribute("errorMsg", errorMsg);
-            
-            
-            response.setContentType("text/html; charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            out.println("<script type='text/javascript'> alert ('check your Search Condition'); </script>");
-            request.getRequestDispatcher("login.jsp").forward(request, response);
-                     
-        }    
+//        } else {
+//            request.setAttribute("errorMsg", errorMsg);
+//
+//
+//            response.setContentType("text/html; charset=UTF-8");
+//            PrintWriter out = response.getWriter();
+//            out.println("<script type='text/javascript'> alert ('check your Search Condition'); </script>");
+//            request.getRequestDispatcher("login.jsp").forward(request, response);
+//
+//        }
 
     
     
