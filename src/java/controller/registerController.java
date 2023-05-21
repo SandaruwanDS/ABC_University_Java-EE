67546591457b9b3e5ce9462cf03ad78b712e8e2b
Sandaruwan <dsrgotest@gmail.com>
2023-05-21 14:01:59 +0530

    update register controller with  commenting the  form validates

--- src/java/controller/registerController.java
+++ src/java/controller/registerController.java
@@ -4,7 +4,7 @@
  */
 package controller;
 
-import beans.DatabaseConnection;
+import bean.DatabaseConnection;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;  
@@ -20,6 +20,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import util.PasswordEncryptor;
 
 
 
@@ -45,19 +46,19 @@ public class registerController extends HttpServlet {
         String encryptedPwd = null;
 
          // Server-side form validation
-        if (userName == null || userName.trim() .isEmpty() ) {
-            errorMsg = "Please enter a valid user name.";
-        } else if (userEmail == null || !userEmail.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")) {
-            errorMsg = "Please enter a valid email address.";
-        } else if (userPhone == null || !userPhone.matches("^\\d{10}$")) {
-            errorMsg = "Please enter a valid phone number.";
-        } else if (userNic == null || !userNic.matches("^\\d{9}[Vv]$")) {
-            errorMsg = "Please enter a valid NIC number.";
-        } else if (userPwd == null || userPwd.length() < 8 ||  userPwd.trim().isEmpty() ) {
-            errorMsg = "Please enter a valid password.";
-        }
-
-        if (errorMsg == null) {
+//        if (userName == null || userName.trim() .isEmpty() ) {
+//            errorMsg = "Please enter a valid user name.";
+//        } else if (userEmail == null || !userEmail.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")) {
+//            errorMsg = "Please enter a valid email address.";
+//        } else if (userPhone == null || !userPhone.matches("^\\d{10}$")) {
+//            errorMsg = "Please enter a valid phone number.";
+//        } else if (userNic == null || !userNic.matches("^\\d{9}[Vv]$")) {
+//            errorMsg = "Please enter a valid NIC number.";
+//        } else if (userPwd == null || userPwd.length() < 8 ||  userPwd.trim().isEmpty() ) {
+//            errorMsg = "Please enter a valid password.";
+//        }
+//
+//        if (errorMsg == null) {
         
         
             
@@ -68,20 +69,22 @@ public class registerController extends HttpServlet {
                     con = DatabaseConnection.connectToDatabase ("jdbc:mysql://localhost/abc_university_p","root","");
                     
                     // Password encryption using MD5
-                     try {  
-                            MessageDigest md = MessageDigest.getInstance("MD5");
-                            md.update(userPwd.getBytes());
-                            byte[] digest = md.digest();
-                            StringBuilder sb = new StringBuilder();
-                            for (byte b : digest) {
-                                sb.append(String.format("%02x", b & 0xff));
-                            }
-                            encryptedPwd = sb.toString();
-                    }   
-                    catch (NoSuchAlgorithmException e)   
-                    {  
-                        e.printStackTrace();  
-                    }  
+                    encryptedPwd = PasswordEncryptor.encryptPassword(userPwd);
+
+//                     try {
+//                            MessageDigest md = MessageDigest.getInstance("MD5");
+//                            md.update(userPwd.getBytes());
+//                            byte[] digest = md.digest();
+//                            StringBuilder sb = new StringBuilder();
+//                            for (byte b : digest) {
+//                                sb.append(String.format("%02x", b & 0xff));
+//                            }
+//                            encryptedPwd = sb.toString();
+//                    }
+//                    catch (NoSuchAlgorithmException e)
+//                    {
+//                        e.printStackTrace();
+//                    }
                     
                     
                     
@@ -109,15 +112,15 @@ public class registerController extends HttpServlet {
 
         
         
-        } else {
-            request.setAttribute("errorMsg", errorMsg);
-            
-            
-            response.setContentType("text/html; charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            out.println("<script type='text/javascript'> alert ('check your Search Condition'); </script>");
-            request.getRequestDispatcher("register.jsp").forward(request, response);
-                     
-        }    
+//        } else {
+//            request.setAttribute("errorMsg", errorMsg);
+//
+//
+//            response.setContentType("text/html; charset=UTF-8");
+//            PrintWriter out = response.getWriter();
+//            out.println("<script type='text/javascript'> alert ('check your Search Condition'); </script>");
+//            request.getRequestDispatcher("register.jsp").forward(request, response);
+//
+//        }
     }
 }
