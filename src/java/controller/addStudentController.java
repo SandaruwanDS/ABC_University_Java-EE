ba175faabd24c4e3da7c932a7adf16b906f10adf
Sandaruwan <dsrgotest@gmail.com>
2023-05-24 03:20:00 +0530

    Update addStudentController.java

--- src/java/controller/addStudentController.java
+++ src/java/controller/addStudentController.java
@@ -5,40 +5,49 @@
  */
 package controller;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import util.DatabaseConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import util.DatabaseConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author PC
  */
-@WebServlet(name = "addStudentController", urlPatterns = {"/addStudent"})
+@WebServlet(name = "addStudentController", urlPatterns = {"/addStudents"})
 public class addStudentController extends HttpServlet {
-
+    Connection conn;
     PreparedStatement pst;
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        String studentReg = request.getParameter("studentReg");
         String studentName = request.getParameter("studentName");
         String studentEmail = request.getParameter("studentEmail");
         String studentPhone = request.getParameter("studentPhone");
         String studentNic = request.getParameter("studentNic");
 
-        Connection conn;
         try {
             conn = DatabaseConnection.connectToDatabase("jdbc:mysql://localhost/abc_university_p", "root", "");
-            //insert data into student table
-            pst = conn.prepareStatement("INSERT INTO student(student_name,student_email,student_phone,student_nic) VALUES (?,?,?,?)");
-            pst.setString(1, studentName);
-            pst.setString(2, studentEmail);
-            pst.setString(3, studentPhone);
-            pst.setString(4, studentNic);
+
+            //insert to student table
+            pst = conn.prepareStatement("INSERT INTO student(student_number,student_name,student_email,student_phone,student_nic) VALUES (?,?,?,?,?)");
+            pst.setString(1, studentReg);
+            pst.setString(2, studentName);
+            pst.setString(3, studentEmail);
+            pst.setString(4, studentPhone);
+            pst.setString(5, studentNic);
             pst.executeUpdate();
 
             conn.close();
